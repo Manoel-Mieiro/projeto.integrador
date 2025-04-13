654ce14245e6415a257d8fa43e631eb1eb8c6167
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-04-13 12:10:11 -0300

    Add form HTML, JavaScript for form submission, and update CSS for layout

---
+++ scripts/form.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const student = document.getElementById("email").value;
+  const meet = document.getElementById("meet").value;
+
+  chrome.storage.local.set({ student: student, meet: meet }, () => {
+    alert("Formulário enviado com sucesso!");
+    window.location.href = "popup.html";
+  });
+});
