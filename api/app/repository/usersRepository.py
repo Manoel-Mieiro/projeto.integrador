5cccc5843ebe89d5899b89a7b60cb414a5e7b5ae
17733635730 <17733635730@cefet-rj.br>
2025-04-24 21:21:43 -0300

    Add user retrieval, update, and deletion functions in users repository

--- api/app/repository/usersRepository.py
+++ api/app/repository/usersRepository.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from db import usersCollection as users
 from app.models.users import Users
 
@@ -11,7 +12,7 @@ def findAllUsers():
             user = Users.from_dict(doc)
             users_list.append(user.to_dict())
 
-        return users_list  
+        return users_list
 
     except Exception as e:
         print("[REPOSITORY]Erro ao buscar users:", e)
@@ -26,4 +27,35 @@ def createUser(data: Users):
         return data.to_dict()
     except Exception as e:
         print("[REPOSITORY]Erro ao criar user:", e)
+        raise e
+
+
+def findOneUser(id: ObjectId):
+    try:
+        return Users(users.find_one({"_id": id})).to_dict()
+    except Exception as e:
+        print("[REPOSITORY]Erro ao buscar user:", e)
+        raise e
+
+
+def updateUser(updatedUser: Users):
+    try:
+        findOneUser(updatedUser._id)
+        users.update_one({"_id": updatedUser._id}, {
+                         "$set": updatedUser.to_dict()})
+
+        return updatedUser.to_dict()
+    except Exception as e:
+        print("[REPOSITORY]Erro ao atualizar user:", e)
+        raise e
+
+
+def deleteUser(id: ObjectId):
+    try:
+        result = users.delete_one({"_id": id})
+        if result.deleted_count == 0:
+            raise ValueError(f"Nenhum usuário encontrado com o id {id}")
+        return {"message": "[REPOSITORY]Usuário removido com sucesso"}
+    except Exception as e:
+        print("[REPOSITORY]Erro ao remover user:", e)
         raise e
