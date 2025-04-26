5e21faa4684ec04bc5b2bf9595252ea5a7762879
17733635730 <17733635730@cefet-rj.br>
2025-04-26 16:54:51 -0300

    Enhance back button functionality to ensure DOM is fully loaded and improve error handling

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,21 @@
-const back = document.getElementById("back");
+// É usado o DOMContentLoaded para garantir que o DOM esteja completamente carregado antes de adicionar o evento de clique ao botão "back". Isso evita erros caso o script seja executado antes do carregamento completo do DOM.
+document.addEventListener("DOMContentLoaded", () => {
+  const back = document.getElementById("back");
 
-back.addEventListener("click", () => {
-  window.location.href = "form.html";
+  if (back) {
+    back.addEventListener("click", () => {
+      chrome.storage.local.remove("state", () => {
+        chrome.runtime.sendMessage({
+          type: "console",
+          message: "Voltar para a página inicial",
+        });
+        window.location.href = "redirect.html";
+      });
+    });
+  } else {
+    chrome.runtime.sendMessage({
+      type: "console",
+      message: "Botão back não foi encontrado",
+    });
+  }
 });
