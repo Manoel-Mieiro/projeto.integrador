52dd51405e0807fa93cbd8fd3eaae8118d5419f7
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-05-10 20:11:58 -0300

    Refactor token request flow to include user existence check and improve error handling

--- scripts/auth/token_request.js
+++ scripts/auth/token_request.js
@@ -1,36 +1,41 @@
 import { styleOnToken } from "./form_styling.js";
+import { fetchUser } from "./fetch_user.js";
 import { CONFIG } from "../config.js";
 import api from "../api.js";
-
 
 export async function triggerTokenRequest(document, components) {
   components.loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-
-    // chamar a API de user para ver se o email existe
-    // se existir, chamar a API de login e salvar o usuário(objeto)
-    // se não existir, mostrar mensagem de erro
     const email = document.getElementById("email").value;
     try {
-      const response = await api.callAPI(
-        "PATCH",
-        `${CONFIG.API_BASE_URL}${CONFIG.LOGIN_ENDPOINT}`,
-        { email: email }
-      );
+      const user = await fetchUser(email);
+      chrome.runtime.sendMessage({ type: "console", message: user });
 
-      if (response && response.newToken) {
-        styleOnToken(components, email);
+      if (user) {
+        const response = await api.callAPI(
+          "PATCH",
+          `${CONFIG.API_BASE_URL}${CONFIG.LOGIN_ENDPOINT}`,
+          { email: email }
+        );
 
-        chrome.storage.session.set({
-          user: email,
-          hasToken: true,
-        });
+        if (response && response.newToken) {
+          styleOnToken(components, email);
+
+          chrome.storage.session.set({
+            user: email,
+            role: user.role,
+            hasToken: true,
+          });
+        } else {
+          alert(`"Ocorreu um erro no servidor"`);
+        }
       } else {
-        alert("Token não recebido. Verifique o email informado.");
+        alert(`${user.email} não está cadastrado`);
+        chrome.storage.session.set({ state: "register" });
+        window.location.href("redirect.html");
       }
     } catch (error) {
-      console.error("Erro ao buscar token:", error);
-      alert("Erro ao conectar com o servidor.");
+      alert(`Erro de conexão com o servidor: ${error}`);
     }
   });
 }
