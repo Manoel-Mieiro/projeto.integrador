e37897d40fc566c41230b703099df5f3edc22b52
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-05-10 20:58:11 -0300

    Refactor redirect logic to handle user roles and improve navigation flow

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,13 +1,12 @@
-chrome.storage.session.get(["state"], (result) => {
-    if (result.state === "logged") {
-      window.location.href = "popup.html";
+chrome.storage.session.get(["state", "role"], (result) => {
+  if (result.state === "logged") {
+    if (result.role === "student") window.location.href = "popup.html";
+    else window.location.href = "lecture.html";
+  } else {
+    if (result.state === "register") {
+      window.location.href = "register.html";
     } else {
-      if(result.state === "register") {
-        window.location.href = "register.html";
-      }
-      else{
-        window.location.href = "form.html";  
-      }
+      window.location.href = "form.html";
     }
-  });
-  
+  }
+});
