81d4104dba3a149b987c2c0b6f0edb8cd5fff359
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-05-10 15:40:20 -0300

    Add validation for required fields in LectureDTO constructor

--- api/app/dto/lectures.py
+++ api/app/dto/lectures.py
@@ -2,6 +2,8 @@ from app.models.lecture import Lecture
 
 class LectureDTO:
     def __init__(self, subject: str, date_lecture: str, period_start: str, period_end: str, teacher: str):
+        if (subject is None or len(subject) == 0) or date_lecture is None or period_start is None or period_end is None or (teacher is None or len(teacher) == 0):
+            raise ValueError("All fields are required")
         self.subject = subject
         self.date_lecture = date_lecture
         self.period_start = period_start
