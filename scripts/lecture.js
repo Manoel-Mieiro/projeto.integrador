e6a34cdf5ead301afeef1f07f6d40f19e9fd74e4
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-05-11 11:59:50 -0300

    Add logout functionality and update lecture form structure; include exit button and event listener

--- scripts/lecture.js
+++ scripts/lecture.js
@@ -1,5 +1,9 @@
 import { fillTeacher } from "./lecture/fields.js";
+import { logout } from "./auth/logout.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const exit = document.getElementById("exit");
   fillTeacher();
+  logout(exit);
+  // é um listener que aguarda o clique para efetuar o logou
 });
