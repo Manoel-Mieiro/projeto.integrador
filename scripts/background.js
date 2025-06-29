265924312a7147afff9d62e3dcc2c1e37b558079
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-06-29 14:49:34 -0300

    Refactor title validation logic in background script to enhance monitoring flow

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,33 @@
 import api from "./api.js";
 import record from "./record.js";
 import { CONFIG } from "./config.js";
+import button from "./button.js";
 
-const lecture = "https://teams.microsoft.com/v2/";
+let regexValidated = false;
+let lastValidatedTitle = "";
 
 async function shouldRecord() {
   const { recording } = await chrome.storage.session.get("recording");
   return recording;
+}
+
+async function validateTitle(tab) {
+  if (!tab.title || tab.title === lastValidatedTitle) return;
+
+  console.log("[validateTitle] tab.title =", tab.title);
+  lastValidatedTitle = tab.title;
+
+  const isValid = record.isTitleValid(tab.title);
+  console.log("VALIDAÇÃO REGEX =>", isValid);
+
+  if (isValid && !regexValidated) {
+    regexValidated = true;
+    await chrome.storage.session.set({ shouldMonitor: true });
+    await chrome.storage.session.set({ entrypoint: tab.id }); //âncora da extensão
+    console.log("Regex válida, ativando monitoramento");
+    await chrome.storage.session.set({ recording: true }); //atualiza recording
+    console.log("GRAVAÇÃO INICIADA")
+  }
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -24,16 +45,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  //busca tab e valida REGEX
+  console.log("[onActivate] VALIDANDO REGEX")
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  await validateTitle(tab)
+
+  // verifica se prossegue com base na validação anterior
+  let flagMonitor = await chrome.storage.session.get(["shouldMonitor"])
+  if(!flagMonitor)return;
+
   if (!(await shouldRecord())) return;
 
   const lecture = await chrome.storage.session.get(["lectureLink"]);
-  const tab = await chrome.tabs.get(activeInfo.tabId);
+  // const tab = await chrome.tabs.get(activeInfo.tabId);
   const student = await record.retrieveUser();
 
   if (tab.url && !tab.url.startsWith(lecture)) {
     console.log(`[onActivated] ${student} left Microsoft Teams tab`);
 
-    const payload = record.buildPayload(tab, lecture.lectureLink, "onActivated", student);
+    const payload = record.buildPayload(
+      tab,
+      lecture.lectureLink,
+      "onActivated",
+      student
+    );
 
     api.callAPI(
       "POST",
@@ -45,6 +80,13 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
+    console.log("[onUpdated] VALIDANDO REGEX")
+    // valida REGEX
+    await validateTitle(tab)
+
+    // verifica se prossegue
+    let flagMonitor = await chrome.storage.session.get(["shouldMonitor"])
+    if(!flagMonitor)return;
     if (!(await shouldRecord())) return;
 
     const lecture = await chrome.storage.session.get(["lectureLink"]);
@@ -53,7 +95,12 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (tab.url && !tab.url.startsWith(lecture)) {
       console.log(`[onUpdated] ${student} left Microsoft Teams tab`);
 
-      const payload = record.buildPayload(tab, lecture.lectureLink, "onUpdated", student);
+      const payload = record.buildPayload(
+        tab,
+        lecture.lectureLink,
+        "onUpdated",
+        student
+      );
 
       api.callAPI(
         "POST",
